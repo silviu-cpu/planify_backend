3097f93d00a1d46932632565f554df28c63489ae
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2021-09-07 22:55:04 +0300

    allor origin23

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,19 @@ var FB = new Facebook({
 });
 
 router.post('/dashboard', (req, res, next) => {
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://planifybackend.herokuapp.com');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
   var user = new User({
     email: req.body.email,
     username: req.body.username,
@@ -32,7 +45,19 @@ router.post('/dashboard', (req, res, next) => {
 
 });
 router.post('/registration', function(req,res,next){
-  
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://planifyacs.netlify.app');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
   var user = new User({
     email: req.body.email,
     username: req.body.username,
@@ -53,6 +78,19 @@ router.post('/registration', function(req,res,next){
 })
 
 router.post('/login', function(req,res,next){
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://planifyacs.netlify.app');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
   let promise = User.findOne({email:req.body.email}).exec();
 
   promise.then(function(doc){
@@ -77,6 +115,19 @@ router.post('/login', function(req,res,next){
 })
 
 router.get('/username', verifyToken, function(req,res,next){
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://planifyacs.netlify.app');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
   return res.status(200).json(decodedToken.username);
 })
 //middleware
