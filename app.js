eabd951e4c5df802edbef3d494167d87bc3f8c0d
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2021-09-07 23:07:48 +0300

    cors

--- app.js
+++ app.js
@@ -16,29 +16,9 @@ mongoose.connect('mongodb+srv://admin:rosfizor@cluster0.2p5ls.mongodb.net/planif
           useUnifiedTopology: true
 })
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://planifybackend.herokuapp.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 //add cors
 var cors = require('cors');
-app.use(cors({origin: 'https://planifybackend.herokuapp.com'}))
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
