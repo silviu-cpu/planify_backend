88032bc9aedb30bcdd4281eb000cbe60f6c2df5a
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2021-09-07 22:40:29 +0300

    allor origin

--- app.js
+++ app.js
@@ -16,18 +16,30 @@ mongoose.connect('mongodb+srv://admin:rosfizor@cluster0.2p5ls.mongodb.net/planif
           useUnifiedTopology: true
 })
 
+// Add headers before the routes are defined
 app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", 
-  "Accept", "x-client-key", "x-client-token", "x-client-secret", "Authorization");
-    next();
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://planifyacs.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
 });
 
 //add cors
 var cors = require('cors');
 app.use(cors())
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
